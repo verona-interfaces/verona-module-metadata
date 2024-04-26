fa4a64f6875eaab7954e5ad3f56a3bd412fd901c
Martin Mechtel <mechtel@iqb.hu-berlin.de>
2024-04-26 11:22:35 +0200

    Update generate_doc.js

--- generate_doc.js
+++ generate_doc.js
@@ -23,7 +23,7 @@ fs.readdirSync(dataFolder).forEach((file) => {
   try {
     let fileContent = fs.readFileSync('./docs/verona-module-metadata.md', 'utf8');
     fileContent = fileContent.replace('```', '\n:::{.column-page}\n```json') + '\n:::';
-    fileContent = fileContent.replace(/^.+\$id/, '---\ntitle: Verona Module Metaadata\n---\n\n<table><tbody><tr><th>\\$id');
+    fileContent = fileContent.replace(/^.+\$id/ms, '---\ntitle: Verona Module Metaadata\n---\n\n<table><tbody><tr><th>\\$id');
     fileContent = fileContent.replace('^[', '\\^[');
     
     fs.writeFileSync('./docs/verona-module-metadata.qmd', fileContent);
